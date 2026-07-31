c99d98d8c6ae25bb21b6e6745a8c20ad44245ed7
Vikash Gautam <vikas1726gh@gmail.com>
2026-07-31 15:38:58 +0530

    Fixed some ES6 features regarding issues for otp on email

--- server/lib/sendMail.ts
+++ server/lib/sendMail.ts
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
